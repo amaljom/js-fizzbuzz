f9cb3717c6d059ca2acbadc36916ed710256d467
amaljom <amaljom99@gmail.com>
2022-06-16 19:21:55 +0200

    asposti gli elementi sulla pagina con un po' di bootstrap

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const fizzBuzzWrapper = document.getElementById('wrapper-fizz-buzz');
 let valueNumber;
 for (let i = 1; i <=100; i++){
+    
 
     if( i % 3 == 0 &&  i % 5 == 0 ){
         console.log('FizzBuzz');
@@ -17,11 +18,11 @@ for (let i = 1; i <=100; i++){
         
     else{
         console.log(i);
-        valueNumber=i
+        valueNumber=i;
     }
     
     const casella = document.createElement('div');
-    casella.classList.add('col-2');
+    casella.classList.add('col-3', 'stile', 'bg-warning', 'm-4', 'text-center');
     casella.innerHTML=(valueNumber);
     fizzBuzzWrapper.append(casella);
 }
