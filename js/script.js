c9a8637c81e544758df00d26f44668ffb96627c8
amaljom <amaljom99@gmail.com>
2022-06-16 19:43:09 +0200

    aggiunto ultimo bonus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const fizzBuzzWrapper = document.getElementById('wrapper-fizz-buzz');
 let valueNumber;
-for (let i = 1; i <=100; i++){
-    
 
+for (let i = 1; i <=100; i++){
     if( i % 3 == 0 &&  i % 5 == 0 ){
         console.log('FizzBuzz');
         valueNumber='FizzBuzz'
@@ -22,10 +21,25 @@ for (let i = 1; i <=100; i++){
     }
     
     const casella = document.createElement('div');
-    casella.classList.add('col-3', 'stile', 'bg-warning', 'm-4', 'text-center');
+    casella.classList.add('col-3', 'stile', 'm-4', 'text-center');
+    if( i % 3 == 0 &&  i % 5 == 0 ){
+        casella.classList.add('bg-primary');
+    }
+        else if( i % 5 == 0){
+            casella.classList.add('bg-danger');
+        }
+        else if(i % 3 == 0){
+            casella.classList.add('bg-info');
+        }
+        
+    else{
+        casella.classList.add('bg-warning');
+    }
+    
     casella.innerHTML=(valueNumber);
     fizzBuzzWrapper.append(casella);
 }
+
     
 
 
